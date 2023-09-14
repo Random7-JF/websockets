1fd7ca1032cf6d4a5f11d5eab775c78ffec1d17e
Jon Fingas <random@pop-os.localdomain>
2023-09-13 21:26:12 -0500

    server working

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const express = require('express');
-const server = require('http').createServer();
+const expressServer = require('http').createServer();
 const app = express()
 
+
+// http server
 app.get('/', function(req, res){
     res.sendFile('index.html', {root: __dirname});
 });
 
-server.on('request', app);
-server.listen(3000, function(){ console.log("server started on port 3000");})   
+expressServer.on('request', app);
+expressServer.listen(3000, function(){ console.log("server started on port 3000");})   
+
+
+// Webscokets
+const WebScoketServer = require('ws').Server;
+const wss = new WebScoketServer({server: expressServer})
+
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size
+    console.log('New Client Connected - Total Clients Connected: ', numClients)
+
+    wss.broadcast(`Current Connections: ${numClients}`);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome to the server.');
+    }
+
+    ws.on('close', function close(){
+        wss.broadcast(`Current Connections: ${numClients}`);
+        console.log("Client closed")
+    })
+
+})
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client){
+        client.send(data)
+    })
+}
