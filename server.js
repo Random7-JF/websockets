5148cbb5302c80897fac85ae6ac2d615196f2d28
Jon Fingas <jon.fingas@gmail.com>
2023-09-14 18:28:15 -0500

    db added

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const expressServer = require('http').createServer();
 const app = express()
 
 
-// http server
+// Http server
 app.get('/', function(req, res){
     res.sendFile('index.html', {root: __dirname});
 });
 
+
 expressServer.on('request', app);
 expressServer.listen(3000, function(){ console.log("server started on port 3000");})   
 
+process.on("SIGINT", () => {
+    server.close(() => {
+        shutdownDB();
+    })
+})
 
-// Webscokets
+// Webscoket server
 const WebScoketServer = require('ws').Server;
 const wss = new WebScoketServer({server: expressServer})
 
@@ -22,6 +28,9 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to the server.');
     }
     console.log('New Client Connected - Total Clients Connected: ', numClients)
+
+    db.run(`INSERT INTO visitors (count time)
+        VALUES (${numClients}, datetime('now'))`)
 
     wss.broadcast(`Current Connections: ${numClients}`);
     ws.on('close', function close(){
@@ -35,4 +44,29 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client){
         client.send(data)
     })
+}
+
+// Database
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    })
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log("Shutting down db");
+    db.close();
 }
